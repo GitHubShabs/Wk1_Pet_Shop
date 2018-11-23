c44ebf549b39588774b6c62210b82a0c11414dad
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-23 16:51:03 +0000

    6th entry done

--- pet_shop.rb
+++ pet_shop.rb
@@ -23,3 +23,19 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+
+def increase_pets_sold(pet_shop, amount)
+number_sold = pets_sold(pet_shop) + amount
+pet_shop[:admin][:pets_sold] = number_sold
+end
+
+def stock_count
+
+end
+
+# for reference only
+# def test_stock_count
+#   count = stock_count(@pet_shop)
+#   assert_equal(6, count)
+# end
