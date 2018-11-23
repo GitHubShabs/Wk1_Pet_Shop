e97e24b8a44b7a315e3af9c3f9079085d4931028
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-23 16:12:53 +0000

    updated first four

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,31 @@
 def pet_shop_name(shop_name)
   return shop_name [:name]
 end
+
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+
+def add_or_remove_cash(pet_shop, amount)
+  update_amount = total_cash(pet_shop) + amount
+  pet_shop[:admin][:total_cash] = update_amount
+end
+
+
+def add_or_remove_cash__remove(pet_shop, amount)
+  update_amount = total_cash(pet_shop) - amount
+  pet_shop[:admin][:total_cash] = update_amount
+end
+
+
+
+
+
+
+# def test_add_or_remove_cash__remove
+#   add_or_remove_cash(@pet_shop,-10)
+#   cash = total_cash(@pet_shop)
+#   assert_equal(990, cash)
+# end
